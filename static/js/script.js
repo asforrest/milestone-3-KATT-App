46ebef7aa408b19234486daf1c065e42974afbe9
Alexander Forrest <asforrest@icloud.com>
2021-01-15 10:08:19 +0000

    Fixed timer / Added Homepage

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
 // giving by Bakudan and later edited by LWC. Here is the original source:
 // https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
 
-// HIDE TIMER, SHOW TIMER AND RESET TO 0
+// CASE STUDY: The following also presents and interesting case as the original code
+// counted the seconds inline. It was a case of refactoring going to far and losing
+// some of the functionality.
+
+// By splitting it out I was able to create a 'running' variable and from there ther
+// buttons could control it by either turning 'running' on or off.
 
 var sec = 0;
     function pad ( val ) { return val > 9 ? val : "0" + val; }
@@ -18,71 +23,84 @@ var sec = 0;
         $("#minutes").html(pad(parseInt(sec/60,10)));
     }, 1000);
 
-
 var running = false
 
-function restart_timer(){
+// Creation of running functions
+
+function restartTimer(){
     running = true;
     sec = 0;
 }
 
+function stopTimer(){
+    running = false;
+    sec = 0;
+}
+
+// Stopwatch button triggers
+
+function currentlyStopped() {
+    document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-ban"></i> No Activity Selected</h1>';
+    stopTimer()
+}
+
 function currentlyWorking() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-briefcase"></i>  Working</h1>';
-    restart_timer()
-}
+    restartTimer()
 }
 
 function currentlySleeping() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-bed"></i>  Sleep</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyChildcaring() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-baby"></i>  Childcare</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyStudying() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-graduation-cap"></i>  Study</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyStrengthing() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-dumbbell"></i>  Strength</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyCardioing() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-running"></i>  Cardio</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyMusicing() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-guitar"></i>  Play Music</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyCleaning() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-soap"></i>  Cleaning</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyTraveling() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-compass"></i>  Travel</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyWatching() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-film"></i>  Watching</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyVideogaming() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-gamepad"></i>  Videogames</h1>';
-    restart_timer()
+    restartTimer()
 }
 
 function currentlyOthering() {
     document.getElementById("current_activity").innerHTML = '<h1 style="font-weight: 600;"><i class="fas fa-random"></i>  Other</h1>';
-    restart_timer()
+    restartTimer()
 }
+
